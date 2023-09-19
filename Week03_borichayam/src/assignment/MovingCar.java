b2d5239634fd30ba87e4380e141167a0d66753e0
borichayam <shs6817@duksung.ac.kr>
2023-09-19 15:53:23 +0900

    복습 필요~~

--- Week03_borichayam/src/assignment/MovingCar.java
+++ Week03_borichayam/src/assignment/MovingCar.java
@@ -1,5 +1,9 @@
 package assignment;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,39 +12,52 @@ import javax.swing.JPanel;
 
 public class MovingCar extends JFrame {
 
-	int img_x=150, img_y=150;
+	int img_x=0;
 	JButton leftB, rightB;
 	
 	public MovingCar() {
 		setSize(600, 300);
-		
-		JPanel moveP = new JPanel();
 		JPanel buttonP = new JPanel();
+		setSize(600, 200);
+		setLayout(new BorderLayout());
 		
 		ImageIcon icon = new ImageIcon("src//car.png");
 		JLabel carL = new JLabel();
 		carL.setIcon(icon);
 		carL.setSize(200,100);
-		carL.setLocation(img_x, img_y);
+		carL.setLocation(img_x, carL.getY());
+		carL.setHorizontalAlignment(JLabel.CENTER);
 		
-		JPanel panel = new JPanel();
 		leftB = new JButton("LEFT");
 		rightB = new JButton("RIGHT");
-		panel.setLayout(null);
 		
-		moveP.add(carL);
+		leftB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                img_x -= 10;
+                carL.setLocation(img_x, carL.getY());
+            }
+        });
+
+        rightB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                img_x += 10;
+                carL.setLocation(img_x, carL.getY());
+            }
+        });
+		
 		buttonP.add(leftB);
 		buttonP.add(rightB);
 		
+		add(carL, BorderLayout.CENTER);
+        add(buttonP, BorderLayout.SOUTH);
 		
-		});
-		add(panel);
 		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
 	public static void main(String[] args) {
 		MovingCar f = new MovingCar();
 	}
-
 }
